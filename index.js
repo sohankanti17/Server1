d1722ecc18394ee7a147c51fdaf9a3a4f3aaa596
Sohan Kanti <30533599+sohankanti17@users.noreply.github.com>
2019-07-05 23:12:16 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 const express = require('express')
 const app = express()
+const port = process.env.port || 3000
 
-app.use('/', (req, res) => {
+app.get('', (req, res) => {
 	res.json({
 		message : "Successfull connection to the server"
 	})
 })
 
-app.use('/aanchal', (req, res) => {
+app.get('/aanchal', (req, res) => {
 	res.json({
 		message : "Successfull connection to the endpoint /aanchal",
 		user: "Aanchal",
 	})
 })
 
-app.use('/sohan', (req, res) => {
+app.get('/sohan', (req, res) => {
 	res.json({
 		message : "Successfull connection to the endpoint /sohan",
 		user: "Sohan Kanti"
 	})
 })
 
-app.use('*', (req, res) => {
+app.get('*', (req, res) => {
 	res.json({
 		message : "Invalid endpoint!"
 	})
 })
 
-app.listen(80, () => {
-    console.log(`Server running on port 80`)
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
 })
